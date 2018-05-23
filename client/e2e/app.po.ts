c4ba11fac7b1c0a86d5ec2e18916e9aa8800cbb4
markelor <mendimarkel@gmail.com>
2018-05-23 11:01:10 +0200

    translate

--- client/e2e/app.po.ts
+++ client/e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, by, element } from 'protractor';
+import { browser, element, by } from 'protractor';
 
-export class AppPage {
+export class AngularPage {
   navigateTo() {
     return browser.get('/');
   }

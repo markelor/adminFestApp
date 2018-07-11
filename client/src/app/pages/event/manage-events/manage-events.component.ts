e19deb54a37287645a51a922a445820807f155cb
markelor <mendimarkel@gmail.com>
2018-07-11 19:09:06 +0200

    pagination

--- client/src/app/pages/event/manage-events/manage-events.component.ts
+++ client/src/app/pages/event/manage-events/manage-events.component.ts
@@ -14,6 +14,7 @@ import { Subscription } from 'rxjs/Subscription';
 export class ManageEventsComponent implements OnInit {
   private subscriptionLanguage: Subscription;
   private events;
+  private page :number = 1;
   constructor(
   	private eventService:EventService,
   	private authService:AuthService,
